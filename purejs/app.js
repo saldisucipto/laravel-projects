b4b096fedd36c5fb0a7ab043b29dede7bd6088f6
Saldi Sucipto <saldisucipto@gmail.com>
2020-11-05 15:25:35 +0700

    With Vue Js

--- purejs/app.js
+++ purejs/app.js
@@ -1,26 +1,46 @@
-// select button 
-const buttonEl = document.querySelector('button');
+// Mencoba dengan vuejs
+// Instance vue 
 
-// input element 
-const inputEl = document.querySelector('input');
+var app = new Vue({
+    el: '#app',
+    data: {
+        tujuan : [],
+        masukanValue : '',
+    },
+    methods: {
+        tambahTujuan(){
+            this.tujuan.push(this.masukanValue);
+            this.masukanValue = '';
+        },
+        clearTujuan(){
+            this.tujuan = [];
+        }
+    },
+})
 
-// list element
-const listEl = document.querySelector('ul');
+// // select button 
+// const buttonEl = document.querySelector('button');
 
-function tambahTujuan(){
-    // value inputan
-    // Menapatakan value inputan dari input tag
-    const masukanValue = inputEl.value;
-    // ekstarct valeu 
-    // membuat sebuah list item di dalam ul
-    const listItemEl = document.createElement('li');
-    // value li sama dengan inputan dari input 
-    listItemEl.textContent = masukanValue;
-    // metho appendchild berguna untuk menambhkan element baru pada html
-    listEl.appendChild(listItemEl);
-    // Merest jetika value sudah di tambahkan ke ke element
-    inputEl.value = '';
-}
+// // input element 
+// const inputEl = document.querySelector('input');
 
-// Event ketika button klik di tekan
-buttonEl.addEventListener('click', tambahTujuan)
+// // list element
+// const listEl = document.querySelector('ul');
+
+// function tambahTujuan(){
+//     // value inputan
+//     // Menapatakan value inputan dari input tag
+//     const masukanValue = inputEl.value;
+//     // ekstarct valeu 
+//     // membuat sebuah list item di dalam ul
+//     const listItemEl = document.createElement('li');
+//     // value li sama dengan inputan dari input 
+//     listItemEl.textContent = masukanValue;
+//     // metho appendchild berguna untuk menambhkan element baru pada html
+//     listEl.appendChild(listItemEl);
+//     // Merest jetika value sudah di tambahkan ke ke element
+//     inputEl.value = '';
+// }
+
+// // Event ketika button klik di tekan
+// buttonEl.addEventListener('click', tambahTujuan)
